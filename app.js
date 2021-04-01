2c73c719cc2b190fb3b5b84d896cb901acadde08
Lana Salah Shamaileh <shamaylehlana62@gmail.com>
2021-04-01 17:37:20 +0300

    Problem4

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) {
   //eslint-disable-line
-  let sum=a+b;
-  return [sum,`The sum of ${a} and ${b} is ${sum}.`]
+let sum=a+b;
+return [sum,`The sum of ${a} and ${b} is ${sum}.`]
 }
-// Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+// Here is the test for sum(); uncomment it to run 
+sum(4, 7);
+//testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -72,11 +73,17 @@ let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) {
   //eslint-disable-line
+  var total = 0;
+  total = sum(sumArr[0], sumArr[1])[0];
+  total = sum(total, sumArr[2])[0];
+   
+   return [total ,sumArr +' was passed in as an array of numbers, and ' + total + ' is their sum.'] ;
+
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
