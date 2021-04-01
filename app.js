1b2db5447e7224ecb07df9b45b5754b7318aecae
Haneenabonser <aabonser@gmail.com>
2021-04-02 02:06:29 +0300

    finished problem5

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ function sumArray(sumArr) {
 
 // Here is the test for sumArray(); uncomment it to run it
 testSumArray(testArray);
-// console.log(sumArray(testArray));
+
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -99,12 +99,15 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) {
+function multiplyArray(multArr) { 
+  let result = [multiply(multArr[0], multiply(multArr[1],multArr[2])[0])[0], 'The numbers ' + [multArr[0]  , multArr[1] , multArr[2]] + ' have a product of ' + multiply(multArr[0], multiply(multArr[1],multArr[2])[0])[0] + '.']
+  return result;
+
   //eslint-disable-line
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
