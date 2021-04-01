3de5ed4b7b11f4ccba97893a6b505f5dd94d9d23
rymbaniali <rym.ayasrah@hotmail.com>
2021-04-01 13:51:26 +0300

    finished problem2

--- app.js
+++ app.js
@@ -7,32 +7,42 @@ Write a function called sum() that takes in two numbers as arguments and then re
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
-// Write your code here
-function sum(a, b) {
-  //eslint-disable-line
-  let sum=a+b;
-  return [sum,`The sum of ${a} and ${b} is ${sum}.`]
-}
-// Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+// // Write your code here
+// function sum(a, b) {
+//   //eslint-disable-line
+//   let sum=a+b;
+//   return [sum,`The sum of ${a} and ${b} is ${sum}.`]
+// }
+// // Here is the test for sum(); uncomment it to run it
+// testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
 /* Problem 2
-Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called multiply() that 
+takes in two numbers as arguments and
+returns an array 
+where the first element is the product of those numbers,
+and the second element is a string that EXACTLY follows this example
+ and uses the values that were input into the function:
+
+ 
 
 "The product of 5 and 9 is 45."
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) {
+function multiply(a, b) { 
+  let prduct=a*b;
+  return [prduct,`The product of ${a} and ${b} is ${prduct}.`]
+// 
   //eslint-disable-line
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
