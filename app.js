22d8df427e76478b83cee41c9170bc2a3b039c59
rymbaniali <rym.ayasrah@hotmail.com>
2021-04-01 17:54:36 +0300

    finished prob4

--- app.js
+++ app.js
@@ -88,13 +88,16 @@ function sumAndMultiply(a,b,c) {
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4, 7, 5);
+// testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
 /* Problem 4
-Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called sumArray() 
+that takes in an array of numbers as its single argument 
+and then returns an array where the first element is the sum of the numbers in the array, 
+and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 
@@ -105,13 +108,28 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 let testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(sumArr) {
+function sumArray(sumArr) { 
+ let a;
+ let b;
+ let c;
+
+for (let i=0; i<testArray.length;i++){
+  a=testArray[i++];
+  b=testArray[i++];
+  c=testArray[i++];
+}
+
+let firstSum=sum(a,b);
+let totSum=sum(firstSum[0],c);
+console.log(totSum[0]);
+
+return [totSum[0],`${a},${b},${c} was passed in as an array of numbers, and ${totSum[0]} is their sum.`]
   //eslint-disable-line
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -161,4 +179,4 @@ function multiplyAnyArray(dynamicArray) {
 // Here is the test for multiplyArray(); uncomment it to run it
 // testMultiplyAnyArray(testDynamicArray);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! // Submit the link to the repo following the instructions in Canvas.
