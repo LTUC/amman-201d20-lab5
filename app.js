2c4da11533d3a32e802d693b1285489a836c9766
dana-younis <younisdana7@gmail.com>
2021-04-01 17:04:21 +0300

    sec

--- app.js
+++ app.js
@@ -9,13 +9,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 
+
 function sum( a, b) {
   //eslint-disable-line
   let sum=a+b;
+  
   return [sum,`The sum of ${a} and ${b} is ${sum}.`]
 }
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
+
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -37,6 +40,7 @@ function multiply(a, b) {
 // Here is the test for multiply(); uncomment it to run it
 testMultiply(5,9);
 
+
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
@@ -52,11 +56,28 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) {
-  //eslint-disable-line
-}
 
-// Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+//  sum=(sum,c);
+//  multiply=(multiply,c);
+
+ let first=sum(a,b);
+ let firstt=multiply(a,b);
+ 
+ console.log(first);
+ console.log(firstt);
+
+ let d= sum(first[0],c);
+ let m=multiply(firstt[0],c);
+ 
+ console.log(d);
+ console.log(m);
+
+ return [d[0],m[0],`${a} and ${b} and ${c} sum to ${d[0]}.`,`The product of ${a} and ${b} and ${c} is ${m[0]}.`]
+  // eslint-disable-line
+ }
+
+//  Here is the test for sumAndMultiply(); uncomment it to run it
+  testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -74,6 +95,7 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) {
+  
   //eslint-disable-line
 }
 
