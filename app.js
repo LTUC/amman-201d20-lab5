1025ea0cddc94a5b11c9b42386637caa1c9ec086
rymbaniali <rym.ayasrah@hotmail.com>
2021-04-01 15:21:13 +0300

    finished problem3

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // // Write your code here
-// function sum(a, b) {
-//   //eslint-disable-line
-//   let sum=a+b;
-//   return [sum,`The sum of ${a} and ${b} is ${sum}.`]
-// }
+function sum(a, b) {
+  //eslint-disable-line
+
+  let sum = a + b;
+  return [sum, `The sum of ${a} and ${b} is ${sum}.`]
+}
 // // Here is the test for sum(); uncomment it to run it
 // testSum(4, 7);
 
@@ -34,36 +35,60 @@ and the second element is a string that EXACTLY follows this example
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { 
-  let prduct=a*b;
-  return [prduct,`The product of ${a} and ${b} is ${prduct}.`]
-// 
+
+function multiply(a, b) {
+  let prduct = a * b;
+  return [prduct, `The product of ${a} and ${b} is ${prduct}.`]
+  // 
   //eslint-disable-line
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+// testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
 /* Problem 3
-Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
+Write a function called sumAndMultiply() 
+that takes in three numbers as separate arguments 
+and returns an array where the first element is the sum of those three numbers, 
+the second element is the product of those three numbers,  
+and the third and fourth elements are strings that EXACTLY follow this example 
+and use the values that were input into the function:
 
 Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
 
 IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
+Test this function by hand in the console to get it working, 
+and when you think it is finished, uncomment the call for the testSumAndMultiply() function 
+and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) {
-  //eslint-disable-line
+function sumAndMultiply(a,b,c) {
+
+  let numberSum = sum(a, b);
+  let finalSum = sum(numberSum[0],c);
+
+  console.log(numberSum);
+  console.log(finalSum);
+  
+  let numProduct3 = multiply(a,b);
+  let finalPrd = multiply(numProduct3[0],c);
+
+  console.log(numProduct3);
+  console.log(finalPrd);
+
+  console.log(finalSum,finalPrd);
+
+  return [finalSum[0], finalPrd[0],`${a} and ${b} and ${c} sum to ${finalSum[0]}.`,`The product of ${a} and ${b} and ${c} is ${finalPrd[0]}.`]
+  //eslint-disable-line 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
