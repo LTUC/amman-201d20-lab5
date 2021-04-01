603ee1dd820ab719d63ff3468154b2414cd492e3
bahaamq <bahaa.mq@gmail.com>
2021-04-01 14:22:44 +0300

    question3

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) {
   //eslint-disable-line
-  let sum=a+b;
-  return [sum,`The sum of ${a} and ${b} is ${sum}.`]
+  let sum = a + b;
+  return [sum, `The sum of ${a} and ${b} is ${sum}.`]
 }
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
@@ -31,18 +31,22 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) {
   //eslint-disable-line
-  let product=a*b;
-  return [product,`The product of ${a} and ${b} is ${product}.`]
+  let product = a * b;
+  return [product, `The product of ${a} and ${b} is ${product}.`]
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
 /* Problem 3
-Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
+Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where
+ the first element is the sum of those three numbers, 
+ the second element is the product of those three numbers,
+   and the third and fourth elements are strings that EXACTLY follow this
+    example and use the values that were input into the function:
 
 Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
@@ -53,11 +57,26 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) {
+  //Sum execution
+  let sum1 = sum(a, b)
+  let sum2 = sum(sum1[0], c)
+  let sumResult = sum2[0]
+
+  //
+  let product1 = multiply(a, b)
+  let product2 = multiply(product1[0], c)
+  let productResult = product2[0]
+
+  return [sumResult, productResult,
+    `${a} and ${b} and ${c} sum to ${sumResult}.`,
+    `The product of ${a} and ${b} and ${c} is ${productResult}.`
+]
   //eslint-disable-line
 }
 
+
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
