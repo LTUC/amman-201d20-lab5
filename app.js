f9379efd4edb026b43269abd79561561ad6c0d8e
alaa281997 <alaabbas97@gmail.com>
2021-04-01 14:05:55 +0300

    second p2

--- app.js
+++ app.js
@@ -31,15 +31,19 @@ that were input into the function:
 
 "The product of 5 and 9 is 45."
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is finished, 
+uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
 function multiply(a, b) {
   //eslint-disable-line
+  let Mult = a * b ;
+  return [Mult,`The product of ${a} and ${b} is ${Mult}.`];
+
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9)
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
